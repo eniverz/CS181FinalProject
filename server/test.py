760c066bfa142590ab637b2e04e32b47ec3d3e71
cavalry-cl <2948583170@qq.com>
2024-11-29 13:51:32 +0800

    win & lose

--- server/test.py
+++ server/test.py
@@ -1,13 +1,19 @@
 from game.game import Board, GameState
 import numpy as np
+from copy import deepcopy
 
 gs = GameState(4, 2)
-pos = [[i, j] for i in range(17) for j in range(17) if gs.board.posInBoard((i, j))]
-position = [[True if gs.board.posInBoard((i,j)) else False for j in range(17)] for i in range(17)]
-np.set_printoptions(linewidth=200)
-print(pos)
-print(np.array(position))
-print([8,8] in pos)
+print(gs.checkWin())
+x = deepcopy(gs.board.checkerlist[1])
+gs.board.checkerlist[1] = deepcopy(gs.board.checkerlist[0])
+gs.board.checkerlist[0] = x
+print(gs.checkWin())
+# pos = [[i, j] for i in range(17) for j in range(17) if gs.board.posInBoard((i, j))]
+# position = [[True if gs.board.posInBoard((i,j)) else False for j in range(17)] for i in range(17)]
+# np.set_printoptions(linewidth=200)
+# print(pos)
+# print(np.array(position))
+# print([8,8] in pos)
 # gs = GameState(2, 6)
 # gs.board = gs.board.moveChecker((5,1),(5,2),0)
 # # gs.board.debugPlayerCheckers()
