11bea41feeabd1bf173cb82689088f99c827ad41
TropinoneH <mspt5871@protonmail.com>
2024-12-21 22:19:08 +0800

    player change

--- server/game/game.py
+++ server/game/game.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+
 from game.utils import Queue
 
 EMPTY_BOX = -1
@@ -11,14 +12,15 @@ DIR = [(DX[i], DY[i]) for i in range(len(DX))]
 ADJACENT_GT = 0
 MIRROR_GT = 1
 
+
 class Board:
     def __init__(self, board_size, player_num, game_type):
-        '''
+        """
         each player has 1+2+...+board_size checkers
         the board is made up of one upward triangle and one downward triangle
-        '''
-        assert player_num in [1,2,3,6], "player_num should be 1,2,3 or 6"
-        assert game_type in [0,1], "game_type should be either ADJACENT_GT or MIRROR_GT"
+        """
+        assert player_num in [1, 2, 3, 6], "player_num should be 1,2,3 or 6"
+        assert game_type in [0, 1], "game_type should be either ADJACENT_GT or MIRROR_GT"
         self.game_type = game_type
         self.len = board_size * 4 + 1
         self.N = board_size
@@ -26,42 +28,42 @@ class Board:
         self.board = [[EMPTY_BOX for i in range(self.len)] for j in range(self.len)]
         self.checkerlist = [[] for player in range(player_num)]
         self.winstate_pos = [set() for player in range(player_num)]
-        triangle_pos_list = [[] for triagle_id in range(6)] # the positions of the six triangles
+        triangle_pos_list = [[] for triagle_id in range(6)]  # the positions of the six triangles
 
         triangle_id = 0
         for i in range(board_size):
-            for j in range(i+1):
-                pos = (2*board_size+1+i, board_size-1-j)
+            for j in range(i + 1):
+                pos = (2 * board_size + 1 + i, board_size - 1 - j)
                 triangle_pos_list[triangle_id].append(pos)
 
         triangle_id = 1
         for i in range(board_size):
-            for j in range(i+1):
-                pos = (2*board_size-1-i, board_size+j)
+            for j in range(i + 1):
+                pos = (2 * board_size - 1 - i, board_size + j)
                 triangle_pos_list[triangle_id].append(pos)
 
         triangle_id = 2
         for i in range(board_size):
-            for j in range(i+1):
-                pos = (i, 3*board_size-j)
+            for j in range(i + 1):
+                pos = (i, 3 * board_size - j)
                 triangle_pos_list[triangle_id].append(pos)
 
         triangle_id = 3
         for i in range(board_size):
-            for j in range(i+1):
-                pos = (2*board_size-1-i, 3*board_size+1+j)
+            for j in range(i + 1):
+                pos = (2 * board_size - 1 - i, 3 * board_size + 1 + j)
                 triangle_pos_list[triangle_id].append(pos)
 
         triangle_id = 4
         for i in range(board_size):
-            for j in range(i+1):
-                pos = (2*board_size+1+i, 3*board_size-j)
+            for j in range(i + 1):
+                pos = (2 * board_size + 1 + i, 3 * board_size - j)
                 triangle_pos_list[triangle_id].append(pos)
-        
+
         triangle_id = 5
         for i in range(board_size):
-            for j in range(i+1):
-                pos = (4*board_size-i, board_size+j)
+            for j in range(i + 1):
+                pos = (4 * board_size - i, board_size + j)
                 triangle_pos_list[triangle_id].append(pos)
 
         if player_num == 1:
@@ -92,46 +94,45 @@ class Board:
             self.winstate_pos[5] = set(triangle_pos_list[2])
 
         for player_id in range(player_num):
-            for x,y in self.checkerlist[player_id]:
+            for x, y in self.checkerlist[player_id]:
                 self.board[x][y] = player_id
 
     def posInBoard(self, pos):
-        '''
+        """
         check if pos is in the board
-        '''
+        """
         # pos in upside-triangle
-        if pos[1] >= self.N and pos[0] >= self.N and pos[0]+pos[1] <= 5*self.N:
+        if pos[1] >= self.N and pos[0] >= self.N and pos[0] + pos[1] <= 5 * self.N:
             return True
         # pos in downside-triangle
-        if pos[1] <= 3*self.N and pos[0] <= 3*self.N and pos[0]+pos[1] >= 3*self.N:
+        if pos[1] <= 3 * self.N and pos[0] <= 3 * self.N and pos[0] + pos[1] >= 3 * self.N:
             return True
         return False
-    
+
     def posEmpty(self, pos):
-        '''
+        """
         check if pos is in the board and empty
-        '''
+        """
         return self.posInBoard(pos) and self.board[pos[0]][pos[1]] == EMPTY_BOX
-    
+
     def posNotEmpty(self, pos):
-        '''
+        """
         check if pos is in the board and not empty
-        '''
+        """
         return self.posInBoard(pos) and self.board[pos[0]][pos[1]] != EMPTY_BOX
-    
+
     def getPlayerCheckers(self, player_id):
         return self.checkerlist[player_id]
 
     def moveChecker(self, start_pos, end_pos, player_id):
-        '''
+        """
         move the checker that belongs to player_id from start to end
         return the new board after the movement
-        '''
-        assert self.posInBoard(start_pos), f'Start position {start_pos} not in board'
-        assert self.posInBoard(end_pos), f'End position {end_pos} not in board'
-        assert self.board[start_pos[0]][start_pos[1]] == player_id, f'Start position does not belong to {player_id}: {start_pos}->{end_pos}'
-        assert self.board[end_pos[0]][end_pos[1]] == EMPTY_BOX, f'End position is not empty: {start_pos}->{end_pos}'
-
+        """
+        assert self.posInBoard(start_pos), f"Start position {start_pos} not in board"
+        assert self.posInBoard(end_pos), f"End position {end_pos} not in board"
+        assert self.board[start_pos[0]][start_pos[1]] == player_id, f"Start position does not belong to {player_id}: {start_pos}->{end_pos}"
+        assert self.board[end_pos[0]][end_pos[1]] == EMPTY_BOX, f"End position is not empty: {start_pos}->{end_pos}"
 
         newBoard = deepcopy(self)
         newBoard.board[start_pos[0]][start_pos[1]] = EMPTY_BOX
@@ -141,10 +142,10 @@ class Board:
         return newBoard
 
     def nextSteps(self, pos):
-        '''
+        """
         get the next steps of certain position
         return the list of new positions
-        '''
+        """
         possibleList = []
 
         assert self.posInBoard(pos), "pos should be inside the board"
@@ -152,7 +153,7 @@ class Board:
 
         # check neighbor
         for dx, dy in DIR:
-            npos = (pos[0]+dx, pos[1]+dy)
+            npos = (pos[0] + dx, pos[1] + dy)
             if self.posEmpty(npos):
                 possibleList.append(npos)
 
@@ -165,56 +166,56 @@ class Board:
             cur = q.pop()
             for dx, dy in DIR:
                 if self.game_type == ADJACENT_GT:
-                    midpos = (cur[0]+dx, cur[1]+dy)
+                    midpos = (cur[0] + dx, cur[1] + dy)
                     if midpos != pos and self.posNotEmpty(midpos):
-                        npos = (midpos[0]+dx, midpos[1]+dy)
+                        npos = (midpos[0] + dx, midpos[1] + dy)
                         if self.posEmpty(npos) and npos not in vis:
                             possibleList.append(npos)
                             q.push(npos)
                             vis.add(npos)
                 elif self.game_type == MIRROR_GT:
-                    midpos = (cur[0]+dx, cur[1]+dy)
+                    midpos = (cur[0] + dx, cur[1] + dy)
                     steps = 1
                     flag = True
-                    while self.posInBoard((midpos[0]+steps*dx, midpos[1]+steps*dy)):
+                    while self.posInBoard((midpos[0] + steps * dx, midpos[1] + steps * dy)):
                         if midpos != pos and self.posNotEmpty(midpos):
                             for t in range(1, steps):
-                                tpos = (midpos[0]+t*dx, midpos[1]+t*dy)
+                                tpos = (midpos[0] + t * dx, midpos[1] + t * dy)
                                 if tpos != pos and self.posNotEmpty(tpos):
                                     flag = False
                                     break
                             if not flag:
                                 break
-                            npos = (midpos[0]+steps*dx, midpos[1]+steps*dy)
+                            npos = (midpos[0] + steps * dx, midpos[1] + steps * dy)
                             if self.posEmpty(npos) and npos not in vis:
                                 possibleList.append(npos)
                                 q.push(npos)
                                 vis.add(npos)
                             break
-                        midpos = (midpos[0]+dx, midpos[1]+dy)
+                        midpos = (midpos[0] + dx, midpos[1] + dy)
                         steps += 1
-
 
         # print(pos, possibleList)
         return possibleList
 
     def checkWin(self, player_id):
-        '''
+        """
         check whether player_id has won
-        '''
+        """
         return len(set(self.checkerlist[player_id]) & self.winstate_pos[player_id]) == len(self.winstate_pos[player_id])
 
     def checkLose(self, player_id):
-        '''
+        """
         check whether player_id loses by having no available move
-        '''
-        assert True, 'This function should not be called, since checking len(nextGameStates)==0 \
-                      when calling nextGameState should be a better approach.'
+        """
+        assert (
+            True
+        ), "This function should not be called, since checking len(nextGameStates)==0 \
+                      when calling nextGameState should be a better approach."
         for pos in self.checkerlist[player_id]:
             if len(self.nextSteps(pos)) > 0:
                 return True
         return False
-
 
     def debugPlayerCheckers(self):
         for player_id in range(self.player_num):
@@ -223,19 +224,20 @@ class Board:
                 print(pos)
             print()
 
+
 class GameState:
-    def __init__(self, board_size:int, player_num:int, game_type:int=ADJACENT_GT):
-        '''
+    def __init__(self, board_size: int, player_num: int, game_type: int = ADJACENT_GT):
+        """
         gamestate is made up of a board and the currnet player id
-        '''
+        """
         self.board = Board(board_size, player_num, game_type)
         self.curPID = 0
         self.game_type = game_type
-    
+
     def nextGameStates(self):
-        '''
+        """
         get the list of possible next game states
-        '''
+        """
         b = self.board
         curPID = self.curPID
         nxtPID = (self.curPID + 1) % b.player_num
@@ -251,4 +253,8 @@ class GameState:
 
     def checkWin(self):
         return self.board.checkWin(self.curPID)
-    
+
+    def moveChecker(self, start_pos, end_pos):
+        self.board = self.board.moveChecker(start_pos, end_pos, self.curPID)
+        self.curPID = (self.curPID + 1) % self.board.player_num
+
--- server/main.py
+++ server/main.py
@@ -33,21 +33,20 @@ def get_all_checker_position(player_num: int):
 
 @app.get("/checker/available_pos")
 def get_available_pos(x: int, y: int):
-    if (not game.board.posInBoard((x, y))) or game.board.posEmpty((x, y)):
+    if (not game.board.posInBoard((x, y))) or game.board.posEmpty((x, y)) or not game.board.board[x][y] == game.curPID:
         raise HTTPException(status_code=HttpStatus.HTTP_400_BAD_REQUEST[0], detail=f"Position ({x}, {y}) is not a player checker")
     return Result.ok(data=game.board.nextSteps((x, y)))
 
 
 @app.post("/checker/move")
-def move_checker(start_x: int, start_y: int, end_x: int, end_y: int, player_id: int):
+def move_checker(start_x: int, start_y: int, end_x: int, end_y: int):
     if (not game.board.posInBoard((start_x, start_y))) or (not game.board.posInBoard((end_x, end_y))):
         raise HTTPException(status_code=HttpStatus.HTTP_400_BAD_REQUEST[0], detail=f"Position ({start_x}, {start_y}) or ({end_x}, {end_y}) is not in board")
     if game.board.posEmpty((start_x, start_y)):
         raise HTTPException(status_code=HttpStatus.HTTP_400_BAD_REQUEST[0], detail=f"Position ({start_x}, {start_y}) is empty")
     if game.board.posNotEmpty((end_x, end_y)):
         raise HTTPException(status_code=HttpStatus.HTTP_400_BAD_REQUEST[0], detail=f"Position ({end_x}, {end_y}) is not empty")
-    if game.board.board[start_x][start_y] != player_id:
-        raise HTTPException(status_code=HttpStatus.HTTP_400_BAD_REQUEST[0], detail=f"Position ({start_x}, {start_y}) does not belong to player {player_id}")
-    new_game = game.board.moveChecker((start_x, start_y), (end_x, end_y), player_id)
-    game.board = new_game
-    return Result.ok(data=new_game)
+    if game.board.board[start_x][start_y] != game.curPID:
+        raise HTTPException(status_code=HttpStatus.HTTP_400_BAD_REQUEST[0], detail=f"Position ({start_x}, {start_y}) does not belong to player {game.curPID}")
+    game.moveChecker((start_x, start_y), (end_x, end_y))
+    return Result.ok(data=game.board.board)
